cee2bf64b98269102b5594100e78894ea7acdc65
Gustav Sörnäs <gustav@sornas.net>
2025-02-21 16:27:12 +0100

    ranking on web

--- src/bin/web/main.rs
+++ src/bin/web/main.rs
@@ -1,4 +1,3 @@
-use art_of_rally_leaderboard_api::{Filter, Leaderboard};
 use art_of_rally_leaderboard_utils::{
     fastest_times, get_default_rallys, get_default_users, get_rally_results, split_times,
     table_utils,
@@ -35,7 +34,7 @@ fn index(body: &str) -> String {
     )
 }
 
-fn rally(title: String, header: Vec<String>, rows: Vec<Vec<[String; 3]>>) -> String {
+fn rally(title: String, header: Vec<String>, rows: Vec<Vec<[String; 4]>>) -> String {
     let header_cells =
         header
             .iter()
@@ -48,17 +47,22 @@ fn rally(title: String, header: Vec<String>, rows: Vec<Vec<[String; 3]>>) -> Str
     let mut driver_rows = String::new();
     for row in rows {
         driver_rows += "<tr class=\"times\">\n";
-        for cell in row.iter().map(|[s, _, _]| s) {
+        for cell in row.iter().map(|s| &s[0]) {
             driver_rows += &format!("<td>{cell}</td>\n");
         }
         driver_rows += "</tr>\n";
         driver_rows += "<tr class=\"deltas\">\n";
-        for cell in row.iter().map(|[_, s, _]| s) {
+        for cell in row.iter().map(|s| &s[1]) {
             driver_rows += &format!("<td>{cell}</td>\n");
         }
         driver_rows += "</tr>\n";
         driver_rows += "<tr class=\"cars\">\n";
-        for cell in row.iter().map(|[_, _, s]| s) {
+        for cell in row.iter().map(|s| &s[2]) {
+            driver_rows += &format!("<td>{cell}</td>\n");
+        }
+        driver_rows += "</tr>\n";
+        driver_rows += "<tr class=\"ranks\">\n";
+        for cell in row.iter().map(|s| &s[3]) {
             driver_rows += &format!("<td>{cell}</td>\n");
         }
         driver_rows += "</tr>\n";
@@ -80,19 +84,13 @@ fn rally(title: String, header: Vec<String>, rows: Vec<Vec<[String; 3]>>) -> Str
 fn main() -> Result<(), Whatever> {
     let mut body = String::new();
     let rallys = get_default_rallys();
-    let (platform, users) = get_default_users();
+    let (platform, user_ids, user_names) = get_default_users();
     for (title, rally_settings) in rallys {
         let leaderboards: Vec<_> = rally_settings
             .into_iter()
-            .map(|(stage, group, weather)| Leaderboard {
-                stage,
-                weather,
-                group,
-                filter: Filter::Friends,
-                platform,
-            })
+            .map(|(stage, group, weather)| (stage, weather, group, platform))
             .collect();
-        let results = get_rally_results(&leaderboards, &users)?;
+        let results = get_rally_results(&leaderboards, &user_ids, &user_names)?;
         let (full_times, partial_times) = split_times(&results);
         let (fastest_total, fastest_stages) = fastest_times(&full_times, &results);
 
