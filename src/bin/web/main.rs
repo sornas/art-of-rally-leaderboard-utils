699da4a38ec7b54cc1d03a8cb330068bca895df9
Gustav Sörnäs <gustav@sornas.net>
2025-08-30 21:20:24 +0200

    flatten big table into small tables

--- src/bin/web/main.rs
+++ src/bin/web/main.rs
@@ -1,12 +1,12 @@
+use art_of_rally_leaderboard_api::car_name;
 use art_of_rally_leaderboard_utils::{
     Rally, fastest_times, get_default_rallys, get_default_users, get_rally_results, split_times,
-    table_utils,
+    table_utils::{format_delta, format_time},
 };
 use maud::{PreEscaped, html};
 use snafu::Whatever;
 
-fn index(body: &[PreEscaped<String>]) -> PreEscaped<String> {
-    let updated = chrono::Utc::now().format("%F %R %Z");
+fn index(body: impl IntoIterator<Item = PreEscaped<String>>) -> PreEscaped<String> {
     html!(
         (maud::DOCTYPE)
         html {
@@ -26,53 +26,121 @@ fn index(body: &[PreEscaped<String>]) -> PreEscaped<String> {
                 }
 
                 p {
-                    "last updated: " (updated)
+                    "last updated: " (chrono::Utc::now().format("%F %R %Z"))
                 }
-            }
-        }
-    )
-}
-
-fn rally(title: String, header: Vec<String>, rows: Vec<Vec<[String; 5]>>) -> PreEscaped<String> {
-    html!(
-        h2 { (title) }
-        table {
-            thead {
-                @for h in header {
-                    th { (h) }
-                }
-            }
-            @for row in rows {
-                tr class="times"         { @for cell in &row { td { (cell[0]) } } }
-                tr class="deltas"        { @for cell in &row { td { (cell[1]) } } }
-                tr class="cars"          { @for cell in &row { td { (cell[2]) } } }
-                tr class="ranks"         { @for cell in &row { td { (cell[3]) } } }
-                tr class="delta-percent" { @for cell in &row { td { (cell[4]) } } }
             }
         }
     )
 }
 
 fn main() -> Result<(), Whatever> {
-    let mut body = Vec::new();
     let rallys = get_default_rallys();
     let (platform, user_ids, user_names) = get_default_users();
+    let mut parts = Vec::new();
+
     for Rally { title, stages } in rallys {
-        let leaderboards: Vec<_> = stages.into_iter().map(|stage| (stage, platform)).collect();
+        let leaderboards: Vec<_> = stages.iter().map(|stage| (*stage, platform)).collect();
         let results = get_rally_results(&leaderboards, &user_ids, &user_names)?;
         let (full_times, partial_times) = split_times(&results);
         let (fastest_total, fastest_stages) = fastest_times(&full_times, &results);
 
-        let (header, rows) = table_utils::stages(
-            &results.stages,
-            &full_times,
-            &partial_times,
-            fastest_total,
-            &fastest_stages,
-        );
-        body.push(rally(title, header, rows));
+        parts.push(html!(h2 { (title) }));
+        // Total results table for each rally. (stages) x (drivers).
+        parts.push(html!(
+            table class="rally" {
+                thead {
+                    th { "driver" }
+                    th { }
+                    th { "total" }
+                    @for (stage, _group, weather) in &stages {
+                        th { (stage) " (" (weather) ")" }
+                    }
+                }
+                @for ft in &full_times {
+                    tr {
+                        td { (ft.user_name) }
+                        td { }
+                        @let total = ft.total_time;
+                        @let fastest_total = fastest_total.unwrap();
+                        @if total == fastest_total {
+                            td class="fastest" { (format_time(total, true)) }
+                        } @else {
+                            td { (format_delta(total, fastest_total, true)) }
+                        }
+                        @for (i, time) in ft.stage_times.iter().copied().enumerate() {
+                            @let fast = fastest_stages[i].unwrap();
+                            @if time == fast {
+                                td class="fastest" { (format_time(time, false)) }
+                            } @else {
+                                td { (format_delta(time, fast, false)) }
+                            }
+                        }
+                    }
+                }
+                @for pt in &partial_times {
+                    tr {
+                        td { (pt.user_name) }
+                        td { "*" }
+                        @let total = pt.total_time;
+                        td { (format_time(total, true)) }
+                        @for (i, time) in pt.stage_times.iter().copied().enumerate() {
+                            @if let Some(time) = time {
+                                @let fast = fastest_stages[i].unwrap();
+                                @if time == fast {
+                                    td class="fastest" { (format_time(time, false)) }
+                                } @else {
+                                    td { (format_delta(time, fast, false)) }
+                                }
+                            } @else {
+                                td { }
+                            }
+                        }
+                    }
+                }
+            }
+        ));
+
+        // For each driver, in-depth stats for each stage
+        for driver in results.driver_results {
+            parts.push(html!(
+                // TODO: link target
+                h3 { (driver.name) }
+                table class="driver" {
+                    thead {
+                        th { "stage" }
+                        th { "time" }
+                        th { "interval" }
+                        th { "car" }
+                        th { "rank" }
+                        th { "world rank" }
+                    }
+                    @for (i, ((stage, group, weather), stage_result)) in stages.iter().zip(driver.stages).enumerate() {
+                        @let Some(stage_result) = stage_result else { continue; };
+                        @let time = stage_result.time_ms;
+                        tr {
+                            td { (stage) " (" (weather) ")" }
+                            td class="time" { (format_time(time, false)) }
+                            @let fast = fastest_stages[i].unwrap();
+                            @if time == fast {
+                                td class="time" { "-:--:--" }
+                            } @else {
+                                td class="time" { (format_delta(time, fast, false)) }
+                            }
+                            td { (car_name(*group, stage_result.car)) }
+                            td { (stage_result.local_rank) }
+                            @if let Some(world_rank) = stage_result.world_rank {
+                                td { (world_rank) }
+                            } @else {
+                                td { }
+                            }
+                        }
+                    }
+                }
+            ));
+        }
     }
-    let html = index(&body).into_string();
+
+    let html = index(parts).into_string();
     println!("{html}");
     Ok(())
 }
